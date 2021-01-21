615de5fc267efae3166b49befd25f0bac644974b
Tarunkumar5 <42748568+Tarunkumar5@users.noreply.github.com>
2021-01-22 00:23:05 +0530

    git@github.com:Tarunkumar5/articlehunt.git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {path:'home',component:HomeComponent}
 ];
-
+//test
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
